3796952545f33dd12e1371728ad179e8f439d1be
MohamedHegab <mohamedhegab92@gmail.com>
2015-10-07 00:10:20 +0200

    changing deploy to deployer in nginx

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,7 +3,7 @@ server '104.131.25.61', port: 22, roles: [:web, :app, :db], primary: true
 
 set :repo_url,        'git@github.com:MohamedHegab/startup.git'
 set :application,     'startup'
-set :user,            'deploy'
+set :user,            'deployer'
 set :puma_threads,    [4, 16]
 set :puma_workers,    0
 
